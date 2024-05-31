bb99f799bdbb46ec7d4274af07a559da57f1589c
LegendtheDestroyer <168696658+LegendtheDestroyer@users.noreply.github.com>
2024-05-31 09:58:55 +0530

    Update queue.py

--- RdxXMusic/utils/inline/queue.py
+++ RdxXMusic/utils/inline/queue.py
@@ -72,9 +72,9 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="❀", url="https://t.me/royal_bot_and_dpz"),
-            InlineKeyboardButton(text="❀", url="https://t.me/ll_about_LEGEND_ll"),
-            InlineKeyboardButton(text="❀", user_id="6229098016")   
+            InlineKeyboardButton(text="❀", url="https://t.me/TitanBotStore"),
+            InlineKeyboardButton(text="❀", url="https://t.me/TitanBotStore"),
+            InlineKeyboardButton(text="❀", user_id="6864577500")   
         ],
         [
             InlineKeyboardButton(
